4679dd84374078384f7024464a666e7805b47474
Status <adm.bulldog@gmail.com>
2024-11-27 21:31:00 +0300

    remove polling, installed req version, check the functionality

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 if [ "$TELEGRAM_BOT_MODE" = "webhook" ]; then
     exec gunicorn --config /opt/app-root/src/llm_bot/api/config/gunicorn.conf.py
-elif [ "$TELEGRAM_BOT_MODE" = "polling" ]; then
-    exec python /opt/app-root/src/llm_bot/api/main.py
 else
     echo "Invalid TELEGRAM_BOT_MODE. Use 'webhook' or 'polling'."
     exit 1
