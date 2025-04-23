de7f55a8086206a234afe4da83db7b7b0ce85950
Igor Tarlinskiy <itarlinskiy@gmail.com>
2025-04-23 10:50:31 +0500

    refactor config

--- src/api/config/gunicorn.conf.py
+++ src/api/config/gunicorn.conf.py
@@ -3,13 +3,13 @@ import signal
 
 from src.configuring.prime import Config
 
+wsgi_app = Config.sgi["WSGI_APP"]
+bind = f"{Config.sgi['HOST']}:{Config.sgi['PORT']}"
+workers = int(Config.sgi["WORKERS_COUNT"])
+worker_class = Config.sgi["WORKER_CLASS"]
+reload = bool(Config.sgi["AUTO_RELOAD"])
+timeout = int(Config.sgi["TIMEOUT"])
 
-wsgi_app = Config.sgi.WSGI_APP
-bind = f"{Config.sgi.HOST}:{Config.sgi.PORT}"
-workers = int(Config.sgi.WORKERS_COUNT)
-worker_class = Config.sgi.WORKER_CLASS
-reload = bool(Config.sgi.AUTO_RELOAD)
-timeout = int(Config.sgi.TIMEOUT)
 
 def worker_int(worker):
     os.kill(worker.pid, signal.SIGINT)
